420a4eeb846d48e43755fa62cf6cb23362c1da06
a7872429 <a7872429@gmail.com>
2026-09-09 16:48:48 +0800

    Add four-player room database migration

---
+++ migrations/0003_add_four_player_rooms.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `rooms` ADD `max_players` integer DEFAULT 2 NOT NULL;
+ALTER TABLE `rooms` ADD `player3_name` text;
+ALTER TABLE `rooms` ADD `player3_token` text;
+ALTER TABLE `rooms` ADD `player4_name` text;
+ALTER TABLE `rooms` ADD `player4_token` text;
